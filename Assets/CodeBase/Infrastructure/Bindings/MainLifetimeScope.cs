77e5680a063136ef910f6317b6a402e7e69a9a8d
Sticklime <91189001+Sticklime@users.noreply.github.com>
2024-06-26 10:35:09 +0300

    WindowServices

--- Assets/CodeBase/Infrastructure/Bindings/MainLifetimeScope.cs
+++ Assets/CodeBase/Infrastructure/Bindings/MainLifetimeScope.cs
@@ -1,16 +1,18 @@
 ﻿using CodeBase.Infrastructure.State;
+using CodeBase.Services.Factory;
 using CodeBase.Services.SceneLoader;
 using VContainer;
 using VContainer.Unity;
 
-namespace CodeBase
+namespace CodeBase.Infrastructure.Bindings
 {
-    public class StateMachineLifetimeScope : LifetimeScope
+    public class MainLifetimeScope : LifetimeScope
     {
         protected override void Configure(IContainerBuilder builder)
         {
             builder.Register<IGameStateMachine, StateMachine>(Lifetime.Singleton);
             builder.Register<ISceneLoader, SceneLoader>(Lifetime.Singleton);
+            builder.Register<IUIFactory, UIFactory>(Lifetime.Singleton);
             builder.Register<LoadGameState>(Lifetime.Transient);
         }
     }
---
+++ Assets/CodeBase/Infrastructure/Services/WindowServices/WindowServices.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeBase.Infrastructure.Services.WindowServices
+{
+    public class WindowServices : IWindowServices
+    {
+    }
+
+    public interface IWindowServices
+    {
+        void OpenWindow(WindowType windowType)
+        {
+        }
+    }
+
+    public enum WindowType
+    {
+        Default = 0,
+    }
+}
